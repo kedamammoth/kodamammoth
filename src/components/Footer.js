b9ee7bee5f063b30bb879ffb518a827ad33e08de
kedamammoth <suichukodama@gmail.com>
2025-06-23 19:56:05 +0900

    refactor: xのリンクを更新

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -39,7 +39,7 @@ function Footer() {
             display: 'flex',
             gap: '10px',
           }}>
-          <Anchor href="https://x.com/KedamaPatsukin" target="_blank">
+          <Anchor href="https://x.com/kedamammooth" target="_blank">
             Twitter
           </Anchor>
           <Anchor href="https://github.com/kedamammoth" target="_blank">
@@ -67,8 +67,8 @@ const TextStyle = styled.div`
   position: relative;
   color: rgb(132, 133, 132);
   --pixel-size: 1px;
-  text-shadow:
-    calc(-1 * var(--pixel-size)) calc(-1 * var(--pixel-size)) 0px #0a0a0a,
+  text-shadow: calc(-1 * var(--pixel-size)) calc(-1 * var(--pixel-size)) 0px
+      #0a0a0a,
     var(--pixel-size) var(--pixel-size) 0px #fefefe;
   font-style: italic;
 
@@ -96,9 +96,7 @@ const TextStyle = styled.div`
       );
     background-color: transparent;
     background-size: 2px 2px;
-    background-position:
-      0px 0px,
-      1px 1px;
+    background-position: 0px 0px, 1px 1px;
     background-clip: text;
     color: transparent;
     text-shadow: none;
